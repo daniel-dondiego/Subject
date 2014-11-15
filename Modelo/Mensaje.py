f996dae006f26c1af496dd4e1f415c5931815503
luis <luisoto@ciencias.unam.mx>
2014-11-14 23:23:02 -0600

    Se agrego la clase mensaje

---
+++ Modelo/Mensaje.py
@@ -0,0 +1,24 @@
+#Clase que abstrae la informacion de un usuario
+class Mensaje(object):
+	#constructor 
+	def __init__(self,id,id_remitente,id_destinatario,mensaje,fecha):
+		self.id = id
+		self.__id_remitente = id_remitente
+		self.__id_destinatario = id_destinatario
+		self.__mensaje = mensaje
+		self.__fecha = fecha
+		
+	def get_id(self):
+        return self.id
+	  
+	def get_id_remitente(self):
+		return self.id_remitente
+
+	def get_id_destinatario(self):
+		return self.id_destinatario
+
+	def get_mensaje(self):
+		return self.mensaje
+
+	def get_fecha(self):
+		return self.fecha

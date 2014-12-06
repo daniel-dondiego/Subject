edc36d20d23c20e9f593781fdbe6e494ac7f37e0
Victor <victorzamoragutierrez@gmail.com>
2014-12-05 20:57:12 -0600

    Corregido Conexion.py

--- Controlador/Comandos.py
+++ Controlador/Comandos.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python
 
 import psycopg2
-from Conexion import Conexion
+import Conexion
 
-def ejecuta_comando(conexion, comando_sql):
+def ejecuta_comando(comando_sql):
     '''
     Ejecuta el comando de sql dado
+    comando_sql: el comando a ejecutar
     '''
-    c = conexion.getConexion()
+    c = Conexion.getConexion()
     cur = c.cursor()
     cur.execute(comando_sql)
     c.commit()
 
-def consulta(conexion, consulta):
+def consulta(consulta):
     '''
     Regresa los renglones con los resultados de la consulta
+    consulta: la consulta a realizar
     Returns: una lista de renglones con los resultados de la consulta, para iterar sobre ellos
     '''
-    c = conexion.getConexion()
+    c = Conexion.getConexion()
     cur = c.cursor()
     cur.execute(consulta)
     rows = cur.fetchall()
     return rows
-    
+
+rows = consulta("SELECT id, name, address, salary  from COMPANY")
+for row in rows:
+     print ("ID = ", row[0])
+     print ("NAME = ", row[1])
+     print ("ADDRESS = ", row[2])
+     print ("SALARY = ", row[3], "\n")
+
--- Controlador/Conexion.py
+++ Controlador/Conexion.py
@@ -2,26 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import psycopg2
-class Conexion(object):
+conn = None
     
-    def __init__(self):
-        '''
-        Crea un objeto de tipo conexión 
-        '''
-        self.conn = None
-    
-    def getConexion(self):
-        '''
-        Regresa la conexión a la base y la crea si no existe
-        Returns: la conexión a la base
-        '''
-        if self.conn == None:
-            self.conn = psycopg2.connect(database="prueba", user="postgres", password="proyecto3", host="127.0.0.1", port="5432")
-        return self.conn 
+def getConexion():
+       '''
+       Regresa la conexión a la base y la crea si no existe
+       Returns: la conexión a la base
+       '''
+       global conn
+       if conn == None:
+           conn = psycopg2.connect(database="prueba", user="postgres", password="proyecto3", host="127.0.0.1", port="5432")
+       return conn 
 
-    def cierraConexion(self):
-        '''
-        Cierra la conexión a la base de datos
-        '''
-        if self.conn != None:
-            self.conn.close()
+def cierraConexion():
+       '''
+       Cierra la conexión a la base de datos
+       '''
+       global conn
+       if conn != None:
+          conn.close()

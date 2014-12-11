0a333d5cdb38120c9389596046c241099018c850
root <root@localhost.localdomain>
2014-12-10 18:37:45 -0600

    Añade categoria

--- web-server/Modelo/Categoria.py
+++ web-server/Modelo/Categoria.py
@@ -34,5 +34,19 @@ class Categoria(object):
                 Agrega la categoría a la base de datos.
                 '''
                 s = 'INSERT INTO categoria (nombre) VALUES(' + str(self.nombre) + ')'
-        Comandos.ejecuta_comando(s)
- 
+                Comandos.ejecuta_comando(s)
+
+        def set_id(self, id):
+                '''
+                Actualiza el valor del id de la instancia
+                id: el nuevo id
+                '''
+                self.id = id
+
+        def set_nombre(self, nombre):
+                '''
+                Actualiza el valor del nombre de la categoria
+                nombre: el nuevo nombre de la categoria
+                '''
+                self.nombre = nombre
+                

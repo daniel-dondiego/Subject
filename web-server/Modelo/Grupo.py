984b59ce2053da4bfa1647a8504759c378a4f7f2
Victor <victorzamoragutierrez@gmail.com>
2014-12-17 12:35:17 -0600

    Agrega setters a grupo

--- web-server/Modelo/Grupo.py
+++ web-server/Modelo/Grupo.py
@@ -5,7 +5,7 @@ class Grupo(object):
         Constructor
         id: el id del grupo en la base
         nombre: el nombre del grupo
-        id_usuario: el id del usuario que creó el grupo
+        id_usuario: el id del usuario que creo el grupo
         visibilidad: la visibilidad del grupo
         imagen: la imagen del grupo
         '''
@@ -15,21 +15,79 @@ class Grupo(object):
         self.__visibilidad = visibilidad
         self.__imagen = imagen
 	
+    def __str__(self):
+        '''
+        Regresa una cadena con el nombre del grupo
+        Returns: el nombre del grupo
+        '''
+        return self.__nombre
+
     def get_id(self):
         '''
         Regresa el id del grupo
         Returns: el id del grupo
         '''
-        return self.id
+        return self.__id
 	
     def get_nombre(self):
-	return self.nombre
+	'''
+        Regresa el nombre del grupo
+        Returns: el nombre del grupo
+        '''
+        return self.__nombre
 
     def get_id_usuario(self):
-        return self.id_usuario
+        '''
+        Regresa el id del usuario que creo el grupo
+        Returns: el id del usuario que creo el grupo
+        '''
+        return self.__id_usuario
 
     def get_visibilidad(self):
-	return self.visibilidad
+        '''
+        Regresa la visibilidad del grupo
+        Returns: la visibilidad del grupo
+        '''
+	return self.__visibilidad
 
     def get_imagen(self):
-	return self.imagen
+        '''
+        Regresa la imagen del grupo
+        Returns: la imagen del grupo
+        '''
+	return self.__imagen
+
+    def set_id(self, id):
+        '''
+        Actualiza el valor del id del grupo
+        id: el nuevo id
+        '''
+        self.__id = id
+    
+    def set_nombre(self, nombre):
+        '''
+        Actualiza el nombre del grupo
+        nombre: el nuevo nombre para el grupo
+        '''
+        self.__nombre = nombre
+    
+    def set_id_usuario(self, id_usuario):
+        '''
+        Actualiza el valor del id del administrador del grupo
+        id_usuario: el nuevo id del usuario
+        '''
+        self.__id_usuario = id_usuario
+        
+    def set_visibilidad(self, visibilidad):
+        '''
+        Cambia la visibilidad del grupo
+        visibilidad: la nueva visibilidad del grupo
+        '''
+        self.__visibilidad = visibilidad
+        
+    def set_imagen(self, imagen):
+        '''
+        Cambia la imagen del grupo
+        imagen: la nueva imagen del grupo
+        '''
+        self.__imagen = imagen

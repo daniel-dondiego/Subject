e79b0f2142f8e070c27ea9d7557b6e6c97518645
Miguel Mendoza <migue_060195@ciencias.unam.mx>
2014-12-18 17:47:20 -0600

    Agrega vistas

--- web-server/Vista/js/perfil_scripts.js
+++ web-server/Vista/js/perfil_scripts.js
@@ -1,9 +1,38 @@
 /**
- *
+ * Carga los datos principales de un usuario
  */
+function cargaDatos () {
+	
+	if(window.XMLHttpRequest)
+		peticion_http = new XMLHttpRequest();
+	else if(window.ActiveXObject)
+		peticion_http = new ActiveXObject("Microsoft.XMLHTTP");
 
-$(document).ajaxComplete(function() {
-	var $label = $("#user_name label";
-	var text = $label.text();
-	$label.text(text.replace("Nombre", "my text")); 
-});
+	peticion_http.onreadystatechange = insertaDatos;
+	peticion_http.open('GET', '/static/Nombre.txt', true);
+	peticion_http.send(null);
+
+	function insertaDatos () {
+		if(peticion_http.readyState == 4) {
+      		if(peticion_http.status == 200) {
+        		document.getElementById("user_name").innerHTML = peticion_http.responseText;        		
+        	}
+    	}	
+	}
+}
+
+window.onload = cargaDatos;
+
+    $(function() {
+    // When the testform is submitted…
+    $("#testform").submit(function() {
+        // post the form values via AJAX…
+        var postdata = {name: $("#name").val()} ;
+        $.post('/submit', postdata, function(data) {
+            // and set the title with the result
+            $("#title").html(data['title']) ;
+           });
+        return false ;
+        });
+    });
+

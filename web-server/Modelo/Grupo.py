1f410d5db8ca558a40835ebf400ff79730f68045
Victor <victorzamoragutierrez@gmail.com>
2015-01-01 18:01:48 -0600

    Corrige el existe

--- web-server/Modelo/Grupo.py
+++ web-server/Modelo/Grupo.py
@@ -159,11 +159,11 @@ class Grupo(object):
         s += str(self.__id) + ', ' + str(id_usuario) + ')'
         Comandos.ejecuta_comando(s)
     
-def existe(nombre):
-    '''
-    Nos dice si el grupo con nombre 'nombre' ya existe
-    nombre: el nombre del grupo que queremos saber si existe
-    '''
-    query = "SELECT * FROM grupos WHERE nombre = " + nombre
-    rows = Comandos.consulta(s)
-    return len(rows) != 0
+    def existe(self, nombre):
+        '''
+        Nos dice si el grupo con nombre 'nombre' ya existe
+        nombre: el nombre del grupo que queremos saber si existe
+        '''
+        query = "SELECT * FROM grupos WHERE nombre = " + nombre
+        rows = Comandos.consulta(s)
+        return len(rows) != 0
